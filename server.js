4962c6f0c9a681a3eb21e870c263222c057ffe73
Glitch (personal-lib-hm) <none>
2020-05-21 12:36:20 +0000

    all routes and test work

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var cors        = require('cors');
 const helmet = require('helmet');
+const noCache = require('nocache');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -15,7 +16,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
 app.use(helmet());
-app.use(helmet.noCache());
+app.use(noCache());
 app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}));
 
 app.use(bodyParser.json());
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,16 +36,38 @@ suite('Functional Tests', function() {
   */
 
   suite('Routing tests', function() {
+    let idOne;
+    let idTwo = "000000000000000000000000";
 
 
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .send({
+          title: "Once a Great Notion"
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, 'response should be an object');
+          assert.property(res.body, 'title', 'book object should contain title');
+          assert.equal(res.body.title, "Once a Great Notion");
+          assert.property(res.body, '_id', 'book object should contain _id');
+          idOne = res.body._id;
+          done();
+             })
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .send({})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, 'please submit a title');
+          done();
+        })
       });
       
     });
@@ -54,7 +76,16 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai.request(server)
+        .get('/api/books')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body, 'response should be an array');
+          assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+          assert.property(res.body[0], 'title', 'Books in array should contain title');
+          assert.property(res.body[0], '_id', 'Books in array should contain _id');
+          done();
+        })
       });      
       
     });
@@ -63,11 +94,27 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai.request(server)
+        .get(`/api/books/${idTwo}`)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body, 'no book exists');
+          done();
+        })
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai.request(server)
+        .get(`/api/books/${idOne}`)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, 'response should be an object');
+          assert.property(res.body, 'comments', 'object should contain comments');
+          assert.property(res.body, 'title', 'object should contain title');
+          assert.property(res.body, '_id', 'object should contain _id');
+          assert.isArray(res.body.comments, 'comments should be an array');
+          done();
+        })
       });
       
     });
@@ -76,7 +123,21 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai.request(server)
+        .post(`/api/books/${idOne}`)
+        .send({
+          comment: "more literature than expected"
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, 'response should be an object');
+          assert.property(res.body, 'comments', 'object should contain comments');
+          assert.property(res.body, 'title', 'object should contain title');
+          assert.property(res.body, '_id', 'object should contain _id');
+          assert.isArray(res.body.comments, 'comments should be an array');
+          assert.include(res.body.comments, 'more literature than expected', 'comments should include new comment')
+          done();
+        })
       });
       
     });
